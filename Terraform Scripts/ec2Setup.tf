259e3d5ff93d406e292fae70bb0956ab220800fb
Mahesh <maheshkumarmahi2001@gmail.com>
2024-02-26 12:26:18 +0530

    Updated with Tags

--- Terraform Scripts/ec2Setup.tf
+++ Terraform Scripts/ec2Setup.tf
@@ -27,6 +27,9 @@ resource "aws_security_group" "severs-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "Server-SecurityGroup"
+  }
 }
 
 output "public_ip" {
