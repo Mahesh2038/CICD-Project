1bddb3611af961104b14f34d332025531c0f2f45
Mahesh <maheshkumarmahi2001@gmail.com>
2024-02-26 13:17:11 +0530

    Updated with VPC code

--- Terraform Scripts/ec2Setup.tf
+++ Terraform Scripts/ec2Setup.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "server" {
   instance_type          = "t2.micro"
   key_name               = "loginKey"
   vpc_security_group_ids = ["servers-sg"]
+  subnet_id = aws_subnet.server-vpc-subnet-01.id
   tags = {
     Name = "Server"
   }
@@ -15,6 +16,7 @@ resource "aws_instance" "server" {
 resource "aws_security_group" "severs-sg" {
   name        = "servers-sg"
   description = "Allowing network traffic to instance"
+  vpc_id = aws_vpc.server-vpc.id
   ingress {
     from_port   = 22
     to_port     = 22
@@ -30,6 +32,56 @@ resource "aws_security_group" "severs-sg" {
   tags = {
     Name = "Server-SecurityGroup"
   }
+}
+
+resource "aws_vpc" "server-vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+        Name = "server-VPC"
+    }
+}
+resource "aws_subnet" "server-vpc-subnet-01" {
+    vpc_id = aws_vpc.server-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "Server-subnet-01"
+    }
+}
+
+resource "aws_subnet" "server-vpc-subnet-02" {
+    vpc_id = aws_vpc.server-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1b"
+    tags = {
+        Name = "Server-subnet-02"
+    }
+}
+
+resource "aws_internet_gateway" "server-vpc-ig" {
+    vpc_id = aws_vpc.server-vpc.id
+    tags = {
+        Name = "server-vpc-IG"
+    }
+}
+
+resource "aws_route_table" "server-RT" {
+    vpc_id = aws_vpc.server-vpc.id
+    route = {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.server-vpc-ig.id
+    }
+}
+
+resource "aws_route_table_association" "server-rta-subnet-01" {
+    subnet_id = aws_subnet.server-vpc-subnet-01.id
+    route_table_id = aws_route_table.server-RT.id
+}
+resource "aws_route_table_association" "server-rta-subnet-02" {
+    subnet_id = aws_subnet.server-vpc-subnet-02.id
+    route_table_id = aws_route_table.server-RT.id
 }
 
 output "public_ip" {
